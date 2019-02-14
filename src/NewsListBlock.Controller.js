6b1800ceaaae1ae47b4b12786d50bba4bba9587b
Dmitry Titarenko <dmitry.titarenko@graph.uk>
2019-02-14 17:51:34 +0500

    ST-12: use select content dialog with filter to select news folders

--- src/NewsListBlock.Controller.js
+++ src/NewsListBlock.Controller.js
@@ -1,4 +1,4 @@
-angular.module("umbraco").controller("NewsListBlock.Controller", function($scope) {
+angular.module("umbraco").controller("NewsListBlock.Controller", function($scope, dialogService) {
 
 	function Item() {
 		this.dataSource = [];
@@ -6,23 +6,22 @@ angular.module("umbraco").controller("NewsListBlock.Controller", function($scope
 
 	$scope.control.value = $scope.control.value || new Item();
 
-	$scope.control.openOverlay = function(){
-		$scope.control.overlay = {
-			view: "contentpicker",
-			show: true,
-			submit: function(model) {
-				$scope.control.overlay.show = false;
-				$scope.control.overlay = null;
-				$scope.control.value.dataSource = model.selection;
-			},
-			close: function(oldModel) {
-				$scope.control.overlay.show = false;
-				$scope.control.overlay = null;
+	$scope.control.openOverlay = function () {
+		dialogService.treePicker({
+			filterCssClass: 'not-allowed not-published',
+			filter: "NewsListPage",
+			treeAlias: 'content',
+			section: 'content',
+			multiPicker: true,
+			callback: function (data) {
+				if (data) {
+					$scope.control.value.dataSource = data;
+				}
 			}
-		}
-	};
+		});
+	}
 
 	$scope.remove = function(index) {
-        $scope.control.value.dataSource.splice(index, 1);
-    };
+		$scope.control.value.dataSource.splice(index, 1);
+	};
 });
